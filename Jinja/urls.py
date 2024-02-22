45f07ec26b9671b3a40bad8910cb030abd82b387
matirualbert <albertnjoroge50@gmail.com>
2024-02-22 09:21:09 +0300

    initial commit

---
+++ Jinja/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from Jinja import views
+
+urlpatterns = [
+    path('', views.home, name='my_index'),
+    path('', views.about, name='my_about'),
+    path('contact/', views.contact, name='my_contact'),
+    path('',views.gallery, name='my_gallery')
+
+]
